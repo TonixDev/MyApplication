3b1aec5a42f84c36bbf30331bb5914361f867ea3
user <skzkfls@naver.com>
2018-06-05 13:25:19 +0900

    init

--- app/src/main/java/com/example/park/myapplication/MainActivity.java
+++ app/src/main/java/com/example/park/myapplication/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 import java.io.*;
 import java.net.*;
+import java.util.regex.Pattern;
 
 public class MainActivity extends Activity {
     EditText userId, userPwd;
@@ -33,12 +34,14 @@ public class MainActivity extends Activity {
         protected String doInBackground(String... strings) {
             try {
                 String str;
-                URL url = new URL("http://192.168.100.66:8080/webProject/data.jsp");
+//                URL url = new URL("http://192.168.100.66:8080/webProject/data.jsp");
+                URL url = new URL("http://192.168.100.71:8080/Android/login.jsp");
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded charset=UTF-8");
                 conn.setRequestMethod("POST");
                 OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
-                sendMsg = "id="+strings[0]+"&pwd="+strings[1]+"&type="+strings[2];
+
+                sendMsg = "ID="+strings[0]+"&PWD="+strings[1];
                 osw.write(sendMsg);
                 osw.flush();
                 if(conn.getResponseCode() == conn.HTTP_OK) {
@@ -61,6 +64,33 @@ public class MainActivity extends Activity {
             }
             return receiveMsg;
         }
+
+        @Override
+        protected void onPostExecute(String receiveMsg){
+            super.onPostExecute(receiveMsg);
+
+            if(receiveMsg !=null){
+                Log.d("ASYNC", "msg="+receiveMsg);
+            }
+
+            if(receiveMsg.trim().equals("true")) {
+                Toast.makeText(MainActivity.this,"로그인",Toast.LENGTH_SHORT).show();
+                /*로그인 성공시 다음으로 실행될 class지정*/
+                //Intent intent = new Intent(MainActivity.this, 보내고 싶은 액티비티.class);
+                //startActivity(intent);
+                finish();
+            }else if(receiveMsg.trim().equals("false")) {
+                 Toast.makeText(MainActivity.this,"아이디or 비밀번호가 다름",Toast.LENGTH_SHORT).show();
+                 userId.setText("");
+                 userPwd.setText("");
+            }
+            /*else if(receiveMsg.trim().equals("noId")) {
+                Toast.makeText(MainActivity.this,"존재하지 않는 아이디",Toast.LENGTH_SHORT).show();
+                userId.setText("");
+                userPwd.setText("");
+            }*/
+        }
+
     }
 
     View.OnClickListener btnListener = new View.OnClickListener() {
@@ -68,44 +98,31 @@ public class MainActivity extends Activity {
         public void onClick(View view) {
             switch (view.getId()) {
                 case R.id.loginBtn : // 로그인 버튼 눌렀을 경우
-                    String loginid = userId.getText().toString();
-                    String loginpwd = userPwd.getText().toString();
+                    String ID = userId.getText().toString();
+                    String PWD = userPwd.getText().toString();
+                    boolean regId = Pattern.matches("^[a-zA-Z0-9]*$", ID);
+
                     try {
-                        String result  = new CustomTask().execute(loginid,loginpwd,"login").get();
-                        if(result.equals("true")) {
-                            Toast.makeText(MainActivity.this,"로그인",Toast.LENGTH_SHORT).show();
-                            /*로그인 성공시 다음으로 실행될 class지정*/
-                            //Intent intent = new Intent(MainActivity.this, 보내고 싶은 액티비티.class);
-                            //startActivity(intent);
-                            finish();
-                        } else if(result.equals("false")) {
-                            Toast.makeText(MainActivity.this,"아이디or 비밀번호가 다름",Toast.LENGTH_SHORT).show();
-                            userId.setText("");
-                            userPwd.setText("");
-                        } else if(result.equals("noId")) {
-                            Toast.makeText(MainActivity.this,"존재하지 않는 아이디",Toast.LENGTH_SHORT).show();
-                            userId.setText("");
-                            userPwd.setText("");
-                        }
-                    }catch (Exception e) {}
-                    break;
+                            if (ID.length() == 0 || regId == false) {
+                                Toast.makeText(MainActivity.this, "ID는 공백일 수 없으며 숫자와 영어만 가능합니다.", Toast.LENGTH_SHORT).show();
+                                return;
+                            } else if (PWD.length() == 0) {
+                                Toast.makeText(MainActivity.this, "패스워드를 입력해주세요", Toast.LENGTH_SHORT).show();
+                                return;
+                            }else{
+                                new CustomTask().execute(ID,PWD);
+                            }
+
+
+                    }catch (Exception e) {
+
+                    }break;
+
                 case R.id.joinBtn : // 회원가입
-                    String joinid = userId.getText().toString();
-                    String joinpwd = userPwd.getText().toString();
-                    try {
-                        String result  = new CustomTask().execute(joinid,joinpwd,"join").get();
-                        if(result.equals("id")) {
-                            Toast.makeText(MainActivity.this,"존재하는 아이디입니다.",Toast.LENGTH_SHORT).show();
-                            userId.setText("");
-                            userPwd.setText("");
-                        } else if(result.equals("ok")) {
-                            userId.setText("");
-                            userPwd.setText("");
-                            Toast.makeText(MainActivity.this,"회원가입을 축하합니다.",Toast.LENGTH_SHORT).show();
-                        }
-                    }catch (Exception e) {}
-                    break;
-            }
+                    Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                    startActivity(intent);
+
+             }
         }
     };
 }
---
+++ app/src/main/java/com/example/park/myapplication/RegisterActivity.java
@@ -0,0 +1,218 @@
+package com.example.park.myapplication;
+
+import android.app.Activity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import java.util.regex.Pattern;
+
+
+public class RegisterActivity extends Activity {
+
+    EditText textId, textPwd, textName;
+    Button joinBtn, regBtn;
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        textId   = (EditText) findViewById(R.id.userId);    /* 연결을 시작하기전 준비하는 단계*/
+        textPwd  = (EditText) findViewById(R.id.userPwd);
+        textName = (EditText) findViewById(R.id.textName);
+        joinBtn  = (Button) findViewById(R.id.joinBtn);
+        regBtn   = (Button)findViewById(R.id.regBtn);
+
+        joinBtn.setOnClickListener(btnListener);
+        regBtn.setOnClickListener(btnListener);
+
+    }
+    public class CustomTask extends AsyncTask<String, Void, String> {
+        String sendMsg, receiveMsg;
+        @Override
+        protected  void onPreExecute(){
+            super.onPreExecute();
+        }
+        @Override
+        protected String doInBackground(String... strings) {
+            /*
+            * 실제 연결이 진행되는 단계 어플리케이션에서 입력한 ID,PW,NAME 값을 strings에 받음
+            * strings 에 받은 것을 꺼내려면 strings[0]를 사용, 먼저보낸 것은 0, 그 다음 보낸 것은 1 순으로 설정
+            * URL 설정
+            * 목적 Jsp를 strings[0], 보낼 param을 strings[1]로 받음
+            * 받은 strings를 다시 semdMsg를 통해 jsp와 통신
+            * JSP에 연결되어 해당 기능을 수행하고나면 jsp에 out.print("ok");를 통해 안드로이드로 ok 라는 문자열이
+            * 안드로이드의 receiveMsg에 넘어 오게 됨
+            * ok라는 문자열을 공백을 재거 해주고 나서 onPostExecute로 받아서 조건에 맞게 구현
+            * */
+            try {
+
+                String str;
+
+                URL url = new URL("http://192.168.100.71:8080/Android/"+strings[0]);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+                conn.setRequestMethod("POST");
+                OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
+                sendMsg = "param=" + strings[1];
+                System.out.println(sendMsg);
+                System.out.println(strings[0]);
+                System.out.println(strings[1]);
+                osw.write(sendMsg);
+                osw.flush();
+                if (conn.getResponseCode() == conn.HTTP_OK) {
+                    InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
+                    BufferedReader reader = new BufferedReader(tmp);
+                    StringBuffer buffer = new StringBuffer();
+                    while ((str = reader.readLine()) != null) {
+                        buffer.append(str);
+                    }
+                    receiveMsg = buffer.toString();
+
+                } else {
+                    Log.i("통신 결과", conn.getResponseCode() + "에러");
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return receiveMsg;
+        }
+        /*
+        통신 후 recviveMsg를 통해 수행하는 onPostExecute
+        * */
+        @Override
+        protected void onPostExecute(String receiveMsg){
+            super.onPostExecute(receiveMsg);
+
+            if(receiveMsg != null){
+                Log.d("ASYNC","result="+receiveMsg);
+            }
+            if(receiveMsg.trim().equals("ok")){
+                /*
+                * Alert창 만들기
+                * */
+                AlertDialog.Builder alert = new AlertDialog.Builder(RegisterActivity.this);
+                alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();     //닫기
+                    }
+                });
+                alert.setMessage("회원가입을 축하합니다.");
+                alert.show();
+
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+            else if(receiveMsg.trim().equals("true")){
+                AlertDialog.Builder alert = new AlertDialog.Builder(RegisterActivity.this);
+                alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();     //닫기
+                    }
+                });
+                alert.setMessage("중복 된 ID 입니다.");
+                alert.show();
+
+
+            }else{
+                AlertDialog.Builder alert = new AlertDialog.Builder(RegisterActivity.this);
+                alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();     //닫기
+                    }
+                });
+                alert.setMessage("사용 가능한 ID 입니다.");
+                alert.show();
+            }
+        }
+    }
+
+
+    final View.OnClickListener btnListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+
+            EditText textId   = (EditText) findViewById(R.id.textId);
+            EditText textPwd  = (EditText) findViewById(R.id.textPwd);
+            EditText textName = (EditText) findViewById(R.id.textName);
+            String   ID       = textId.getText().toString();
+            String   PWD      = textPwd.getText().toString();
+            String   NAME     = textName.getText().toString();
+            boolean  regId    = Pattern.matches("^[a-zA-Z0-9]*$", ID);
+            boolean  regName  = Pattern.matches("^[a-zA-Z가-힣]*$", NAME);
+
+
+            switch (view.getId()) {
+                case R.id.joinBtn:
+                   try {
+                        if (ID.length() == 0 || regId == false) {
+                            Toast.makeText(RegisterActivity.this, "ID는 공백일 수 없으며 숫자와 영어만 가능합니다.", Toast.LENGTH_SHORT).show();
+                            return;
+                        } else if (PWD.length() == 0) {
+                            Toast.makeText(RegisterActivity.this, "패스워드는 공백일 수 없습니다.", Toast.LENGTH_SHORT).show();
+                            return;
+                        } else if (NAME.length() == 0 || regName == false) {
+                            Toast.makeText(RegisterActivity.this, "이름을 입력해 주세요 (한글,영어)", Toast.LENGTH_SHORT).show();
+                            return;
+                        } else {
+                            String jsp = "insert.jsp";
+                            JSONObject jsonObject = new JSONObject();
+                            jsonObject.put("ID",ID);
+                            jsonObject.put("PWD",PWD);
+                            jsonObject.put("NAME",NAME);
+
+                            new CustomTask().execute(jsp,jsonObject.toString());
+                            /*
+                             ID, PWD, NAME 값을 가지고 DB연결 MEMBER TABLE INSERT
+                            */
+
+                        }
+                    } catch (Exception e) {
+                    }
+                    break;
+
+                case R.id.regBtn:
+
+                    try {
+                        if (ID.length() == 0 || regId == false) {
+                            Toast.makeText(RegisterActivity.this, "ID는 공백일 수 없으며 숫자와 영어만 가능합니다.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }else {
+                            String jsp = "regId.jsp";
+                            new CustomTask().execute(jsp,ID);
+                        }
+                    } catch (Exception e) {
+                    }
+                    break;
+            }
+        }
+    };
+}
+
+
